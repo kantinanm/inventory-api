379bb49026e9cd817f6eb14025f00b740ecfe809
kantinan makmee <kantinanm@nu.ac.th>
2018-09-26 16:27:38 +0700

    update return result 2 kind , in use and discharge

--- util.js
+++ util.js
@@ -149,48 +149,96 @@ exports.test =function(code,cb) {
                 var section_info =[];
                 //7450-010-5429 cutoff
 
-                for(var i=0;i<result.output.length;i++) {
 
-                    var tmp = {
-                        'item-code' : result.output[2].text.trim(),
-                        'item-type' : result.output[4].text.trim(),
-                        'length' : result.output[6].text.trim(),
-                        'kind' : result.output[8].text.trim(),
-                        'name' : result.output[10].text.trim(),
-                        'brand' : result.output[12].text.trim(),
-                        'model' : result.output[14].text.trim(),
-                        'serial' : result.output[16].text.trim(),
-                        'warranty' : result.output[18].text.trim(),
-                        'price' : ((result.output[20].text.replace(/\n|\r/g, "")).trim()).replace(/บาท/, ""),
-                        'unit' : (result.output[22].text.replace(/\n|\r/g, "")).trim(),
-                        'organization' : result.output[26].text.trim(),
-                        'room' : result.output[32].text.trim(),
-                        'procurement' : [{
-                            'method':result.output[34].text.trim(),
-                            'source':result.output[36].text.trim(),
-                            'year':result.output[38].text.trim().trim(),
-                            'docno_withdraw':result.output[40].text.trim(),
-                            'owner_withdraw':result.output[42].text.trim(),
-                            'contact_no':result.output[44].text.trim(),
-                            'contact_date':result.output[46].text.trim(),
-                            'received_date':result.output[48].text.trim(),
-                            'vender':result.output[50].text.replace(/\n|\r/g, "").trim(),
-                            'doc_invoice':result.output[52].text.trim()
-                        }],
-                        'status' : result.output[54].text.trim(),
-                        'appearance' : result.output[56].text.trim(),
-                        'holder' : result.output[58].text.trim(),
-                        'reference' : result.output[60].text.trim()
-                    }
-
-
-                }
 
                 //section_info.push(tmp);
 
+                if(result.output.length==65){
+                    // item in use
+                    for(var i=0;i<result.output.length;i++) {
+
+                        var tmp = {
+                            'item-code' : result.output[2].text.trim(),
+                            'item-type' : result.output[4].text.trim(),
+                            'length' : result.output[6].text.trim(),
+                            'kind' : result.output[8].text.trim(),
+                            'name' : result.output[10].text.trim(),
+                            'brand' : result.output[12].text.trim(),
+                            'model' : result.output[14].text.trim(),
+                            'serial' : result.output[16].text.trim(),
+                            'warranty' : result.output[18].text.trim(),
+                            'price' : ((result.output[20].text.replace(/\n|\r/g, "")).trim()).replace(/บาท/, ""),
+                            'unit' : (result.output[22].text.replace(/\n|\r/g, "")).trim(),
+                            'organization' : result.output[26].text.trim(),
+                            'room' : result.output[32].text.trim(),
+                            'procurement' : [{
+                                'method':result.output[34].text.trim(),
+                                'source':result.output[36].text.trim(),
+                                'year':result.output[38].text.trim().trim(),
+                                'docno_withdraw':result.output[40].text.trim(),
+                                'owner_withdraw':result.output[42].text.trim(),
+                                'contact_no':result.output[44].text.trim(),
+                                'contact_date':result.output[46].text.trim(),
+                                'received_date':result.output[48].text.trim(),
+                                'vender':result.output[50].text.replace(/\n|\r/g, "").trim(),
+                                'doc_invoice':result.output[52].text.trim()
+                            }],
+                            'status' : result.output[54].text.trim(),
+                            'appearance' : result.output[56].text.trim(),
+                            'holder' : result.output[58].text.trim(),
+                            'reference' : result.output[60].text.trim()
+                        }
+
+
+                    }
+                    section_info.push(tmp);
+                }else if(result.output.length==76){
+                    // item discharge
+
+                    for(var i=0;i<result.output.length;i++) {
+
+                        var tmp = {
+                            'item-code' : result.output[2+1].text.trim(),
+                            'item-type' : result.output[4+1].text.trim(),
+                            'length' : result.output[6+1].text.trim(),
+                            'kind' : result.output[8+1].text.trim(),
+                            'name' : result.output[10+1].text.trim(),
+                            'brand' : result.output[12+1].text.trim(),
+                            'model' : result.output[14+1].text.trim(),
+                            'serial' : result.output[16+1].text.trim(),
+                            'warranty' : result.output[18+1].text.trim(),
+                            'price' : ((result.output[20+1].text.replace(/\n|\r/g, "")).trim()).replace(/บาท/, ""),
+                            'unit' : (result.output[22+1].text.replace(/\n|\r/g, "")).trim(),
+                            'organization' : result.output[26+1].text.trim(),
+                            'room' : result.output[32+1].text.trim(),
+                            'procurement' : [{
+                                'method':result.output[34+1].text.trim(),
+                                'source':result.output[36+1].text.trim(),
+                                'year':result.output[38+1].text.trim().trim(),
+                                'docno_withdraw':result.output[40+1].text.trim(),
+                                'owner_withdraw':result.output[42+1].text.trim(),
+                                'contact_no':result.output[44+1].text.trim(),
+                                'contact_date':result.output[46+1].text.trim(),
+                                'received_date':result.output[48+1].text.trim(),
+                                'vender':result.output[50+1].text.replace(/\n|\r/g, "").trim(),
+                                'doc_invoice':result.output[52+1].text.trim()
+                            }],
+                            'status' : result.output[54+1].text.trim(),
+                            'appearance' : result.output[56+1].text.trim(),
+                            'holder' : result.output[58+1].text.trim(),
+                            'reference' : result.output[60+1].text.trim()
+                        }
+
+
+                    }
+                    section_info.push(tmp);
+                }
+
+                //cb({'length':result.output.length});
+
                 //console.log("section_info :"+tmpSchedule);
-                cb(result);//section_info
-                //cb(section_info);
+                //cb(result);//section_info
+                cb(section_info);
             });
 
         });
